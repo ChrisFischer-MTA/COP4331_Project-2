4432a437210926ba32888ff7f9a83a50ee9b433f
David Schneck <david.schneckster@hotmail.com>
2021-10-19 15:13:55 -0400

    fixed the merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,12 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './NavBar.js';
-=======
 //import '../styles/App.css';
 import LoginPage from './LoginPage.js';
->>>>>>> dafd673db06cdad12bf8af0073c02d4578bccd7a
+import NavBar from './NavBar.js';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-	  <NavBar/>
-=======
 	  <LoginPage/>
->>>>>>> dafd673db06cdad12bf8af0073c02d4578bccd7a
+	  <NavBar/>
     </div>
   );
 }
