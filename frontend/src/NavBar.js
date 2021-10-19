676a9c549bcdb533dee28008a09e0a404af07d53
David Schneck <david.schneckster@hotmail.com>
2021-10-19 15:05:56 -0400

    added navbar class

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.id = 1;
+
+	}
+
+	render() {
+		return (
+			<div className="NavBar">
+				<h1>Hello</h1>
+			</div>
+		)
+	}
+
+}
+
+export default NavBar;
