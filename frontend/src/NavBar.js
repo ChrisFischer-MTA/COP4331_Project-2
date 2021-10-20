a9b800c477f1acf34037391010b03b055f0abd53
David Schneck <david.schneckster@hotmail.com>
2021-10-20 13:45:11 -0400

    started work on the nav bar

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import './styles/styles.css';
 
 class NavBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.id = 1;
-
 	}
 
 	render() {
 		return (
-			<div className="NavBar">
-				<h1>Hello</h1>
+			<div className="navbar">
+				<a><i className="icon-comment icon-align-left"></i>Status View</a>
+				<a>Login/Logut</a>
 			</div>
 		)
 	}
-
 }
 
 export default NavBar;
