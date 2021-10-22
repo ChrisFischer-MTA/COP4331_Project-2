058897c580aab1ea2a407ba0e0be031f0aa25a85
David Schneck <david.schneckster@hotmail.com>
2021-10-22 16:16:27 -0400

    added icons to navbar, changed color scheme, and started work on timer

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -2,16 +2,30 @@ import React from 'react';
 import './styles/styles.css';
 
 class NavBar extends React.Component {
+	out_string = <i className="fas fa-sign-out-alt"> Logout</i>;
+	in_string = <i className="fas fa-sign-in-alt"> Login</i>;
+
 	constructor(props) {
 		super(props);
-		this.id = 1;
+		this.loggedIn = props.loggedIn;
+		this.curTime = new Date().toLocaleString();
+    	setInterval(() => {
+		console.log("hi");
+      	this.setState({
+       	 curTime : new Date().toLocaleString(),
+     	 })
+   	 }, 1000)
 	}
+
+	componentDidMount() {
+  	}	
 
 	render() {
 		return (
 			<div className="navbar">
-				<a><i className="icon-comment icon-align-left"></i>Status View</a>
-				<a>Login/Logut</a>
+				<a><i className="fas fa-glasses"></i> Status View</a>
+				<a>{this.curTime}</a>
+				<a>{this.loggedIn ? this.out_string: this.in_string}</a>
 			</div>
 		)
 	}
