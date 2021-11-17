c219f26c4900471c0dffaa7ff59a67563fdfa11e
RPenrose1 <rppenrose@knights.ucf.edu>
2021-11-17 14:01:07 -0500

    admin and navbar updated

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,36 +3,53 @@ import '../styles/admin.css'
 //import {confirmAlert} from 'react-confirm-alert';
 import NavBar from '../NavBar';
 import useState from 'react';
-import { thisExpression } from '@babel/types';
 
-export default class Admin extends React.Component {
-    constructor(props){
+export default class Admin extends React.Component 
+{
+    constructor(props)
+    {
         super(props);
-        this.state = {
+        this.state = 
+        {
             teamNumber: 0,
             servNumber: 0,
             arr: [],
+
             show:false,
             compete: true
         };
     };
+    addField()
+    {
+        this.setState({arr:[...this.state.arr, ""]})
+    }
+    handleChange(e, i)
+    {
+        this.state.arr[i] = e.target.value
+        this.setState({arr:this.state.arr})
+    }
     render() {
         const blankSlate = async event =>
         {
             // API call
             // Indicate current competition is to be wiped  
         };
+
         return(
         <div>
+            <NavBar/><br/><br/><br/><br/>
             <span id = 'inner-title'>Admin Page</span><br/>
+
             {this.state.compete?
             <div>
             <input type="number" id="teamNum" placeholder="Number of teams" />
             <input type="number" id="servNum" placeholder="Number of services" /><br/>
+
             <button id = "newCom" onClick={() => {
                 if(document.getElementById("teamNum").value > 0 & document.getElementById("servNum").value > 0)
                 {
-                    this.setState({
+                    this.setState(
+                    {
                         teamNumber: document.getElementById("teamNum").value,
                         servNumber: document.getElementById("servNum").value,
                         arr: new Array(this.state.servNumber),
@@ -42,20 +59,39 @@ export default class Admin extends React.Component {
                         document.getElementById("servNum").style.display = "none";
                         document.getElementById("teamNum").style.display = "none";
                 }
-                else{
-                    alert("Input fields must have a positive integer")
-                }
-            }}>New Competition
+
+                else
+                {
+                    alert("Input fields must have a positive integer to proceed")
+                }}}>New Competition
             </button>
-            {this.state.show? <div><input id = "service" placeholder = "Service"/></div> : null} 
-        </div>
-        :
-        <div>
+
+            {this.state.show? 
+                <div>
+                    {
+                        this.state.arr.map((service, i) =>{
+                            return(
+                                <div key ={i}>
+                                    <input type="text" onChange={(e)=>this.handleChange(e, i)} value={service}/>
+                                </div>
+                            )
+                        })
+                    }
+                    <button onClick={(e)=>this.addField(e)}>Add thing</button>
+                    <button id = "submit" onClick={
+                        console.log("Placeholder")}>Submit Services
+                    </button>
+
+                </div> : null} 
+
+        </div> : <div>
             <input type="submit" id="BlankSlateButton" value="Delete Competition"
                     onClick={() => { 
                         if (window.confirm('Are you sure you wish to delete this competition?')) 
-                        this.onCancel() } }/>
+                            this.onCancel()
+                    }}/>
         </div>}
+        
         </div>
         );
     }
