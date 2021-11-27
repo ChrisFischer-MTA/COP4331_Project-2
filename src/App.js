d6cddfb80c73b46ad9584442858d959316fc94de
David Schneck <david.schneckster@hotmail.com>
2021-11-27 14:54:46 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Recent from './routes/Recent';
 import AddMachines from './routes/AddMachines';
 
 /* ADMIN */
-import A_Status from './routes/A_Status';
-import A_Service from './routes/A_Service';
-import A_Recent from './routes/A_Recent';
+import A_STATUS from './routes/A_Status';
+import A_SERVICE from './routes/A_Service';
+import A_RECENT from './routes/A_Recent';
 import Team from './routes/Team';
-import Admin from './routes/admin';
+import Admin from './routes/Admin';
 
 import Home from './routes/Home';
 import Registration from './routes/Registration';
@@ -56,13 +56,13 @@ export default class App extends Component {
 						<Route path="/recent" element={<Recent sid={this.state.sid} userType={this.state.userType} />} />
 						<Route path="/addmachines" element={<AddMachines sid={this.state.sid} userType={this.state.userType} />} />
 
-						<Route path="/adminstatus" element={<A_Status sid={this.state.sid} userType={this.state.userType} />} />
-						<Route path="/adminservice" element={<A_Service sid={this.state.sid} userType={this.state.userType} />} />
-						<Route path="/adminrecent" element={<A_Recent sid={this.state.sid} userType={this.state.userType} />} />
+						<Route path="/adminstatus" element={<A_STATUS sid={this.state.sid} userType={this.state.userType} />} />
+						<Route path="/adminservice" element={<A_SERVICE sid={this.state.sid} userType={this.state.userType} />} />
+						<Route path="/adminrecent" element={<A_RECENT sid={this.state.sid} userType={this.state.userType} />} />
 						<Route path="/team" element={<Team sid={this.state.sid} />} />
 						<Route path="/register" element={<Registration />} />
 						<Route path="/login" element={<Login loggedIn={this.state.loggedIn} />} />
-						<Route path="/admin" element={<Admin/>} />
+						<Route path="/admin" element={<Admin sid={this.state.sid}/>} />
 						<Route path='*' element={<NotFound/>} />
 					</Routes>
 				</BrowserRouter>
