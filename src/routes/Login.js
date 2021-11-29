7208c6f432739c01ee61bd70fba02369fd5ccf18
RPenrose1 <rppenrose@knights.ucf.edu>
2021-11-29 13:35:05 -0500

    Login/Register gives error prompts. Register now checks to ensure passwords match

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -45,6 +45,7 @@ export default function Login() {
 		})
 		.catch(error => {
 			console.log("Error:\n", error);
+			alert("An invalid email or password has been provided. Please try again.");
 		});
 	}
 
@@ -72,77 +73,3 @@ export default function Login() {
     )
 
 }
-/*
-export default class Login extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			email: "",
-			password: "",
-		}
-		
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-	}
-	
-	handleSubmit(event) {
-		event.preventDefault(); const {
-			email,
-			password
-		} = this.state;
-
-		axios.post("https://scoring-engine-api.herokuapp.com/api/login", { 
-			email: email, 
-			password: password
-		})
-		.then(response => {
-			console.log(response.data);
-			if (response.data.error === "") {
-				this.props.handleSuccessfulAuth(response.data.sid, false);
-				//this.props.sessionId = response.data.sessionId;
-
-			}
-		})
-		.catch(error => {
-			console.log("Error:\n", error);
-		});
-	}
-
-	handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-
-		event.preventDefault();
-	}
-
-	render() {
-		return (
-			<div>
-				<br/>
-				<h1>Login</h1>
-				<form className="loginForm" onSubmit={this.handleSubmit}>
-					<input type="text" 
-						name="email" 
-						placeholder="Email/Name" 
-						value={this.state.email}
-						onChange={this.handleChange}
-						required
-					/>
-					<input type="password" 
-						name="password" 
-						placeholder="Password" 
-						value={this.state.password}
-						onChange={this.handleChange}
-						required
-					/>
-					<button type="submit">Submit <i className="fas fa-sign-in-alt"></i></button>
-				</form>
-			</div>
-
-		)
-	}
-
-}
-*/
